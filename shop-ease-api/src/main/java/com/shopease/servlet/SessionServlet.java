5236e375a666f2b32366f9172a942263de461353
rajvardhan17 <rajvardhansingh1703@gmail.com>
2026-02-22 01:07:23 +0530

    changes in servlet session

--- shop-ease-api/src/main/java/com/shopease/servlet/SessionServlet.java
+++ shop-ease-api/src/main/java/com/shopease/servlet/SessionServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet("/api/session")
+//@WebServlet("/api/session")
 public class SessionServlet extends HttpServlet {
 
     private final ObjectMapper mapper = new ObjectMapper();
